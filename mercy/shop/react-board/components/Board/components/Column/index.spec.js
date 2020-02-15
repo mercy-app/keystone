4231066c4ba85a48ecf4a27474171938accd1537
I Chen Wu <starchainxz@gmail.com>
2020-02-15 18:05:07 +0800

    trying board

---
+++ mercy/shop/react-board/components/Board/components/Column/index.spec.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Column, { StyledColumn } from './'
+
+describe('<Column />', () => {
+  let subject
+
+  const renderCard = jest.fn((_, { title }) => <div>{title}</div>)
+
+  const column = {
+    id: 1,
+    title: 'Backlog',
+    cards: [
+      {
+        id: 1,
+        title: 'Card title 1',
+        description: 'Card content'
+      },
+      {
+        id: 2,
+        title: 'Card title 2',
+        description: 'Card content'
+      }
+    ]
+  }
+
+  function mount({ children = column, ...otherProps } = {}) {
+    subject = render(
+      <Column {...otherProps} renderColumnHeader={() => <div>Backlog</div>} renderCard={renderCard}>
+        {children}
+      </Column>
+    )
+    return subject
+  }
+
+  function reset() {
+    subject = undefined
+    renderCard.mockClear()
+  }
+
+  beforeEach(reset)
+
+  it('renders a column', () => {
+    expect(mount().container.querySelector('div')).toBeInTheDocument()
+  })
+
+  it("renders the column's header", () => {
+    expect(mount().queryByText(/^Backlog$/)).toBeInTheDocument()
+  })
+
+  describe("about the column's card", () => {
+    beforeEach(() => mount())
+
+    it('renders the specified cards in the column ordered by its specified position', () => {
+      const cards = subject.queryAllByText(/^Card title/)
+      expect(cards).toHaveLength(2)
+      expect(cards[0]).toHaveTextContent(/^Card title 1/)
+      expect(cards[1]).toHaveTextContent(/^Card title 2/)
+    })
+
+    it('calls the "renderCard" passing the column, the card and whether the card is dragging or not', () => {
+      expect(renderCard).toHaveBeenCalledWith(column, expect.objectContaining({ id: 1, title: 'Card title 1' }), false)
+    })
+  })
+})
+
+describe('<StyledColumn />', () => {
+  it('renders a column', () => {
+    expect(render(<StyledColumn />).container.querySelector('div')).toBeInTheDocument()
+  })
+})
---
+++ mercy/shop/react-board/components/Board/components/DefaultColumnHeader/index.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import CursorPointer from '../CursorPointer'
+
+const ColumnHeaderSkeleton = styled.div`
+  padding-bottom: 10px;
+  font-weight: bold;
+  display: flex;
+  justify-content: space-between;
+
+  span:nth-child(2) {
+    cursor: pointer;
+  }
+`
+
+const DefaultButton = styled.button`
+  color: #333333;
+  background-color: #ffffff;
+  border-color: #cccccc;
+
+  :hover,
+  :focus,
+  :active {
+    background-color: #e6e6e6;
+  }
+`
+
+const Input = styled.input`
+  :focus {
+    outline: none;
+  }
+`
+
+function ColumnTitle({ allowRenameColumn, onClick, children: title }) {
+  return allowRenameColumn ? <CursorPointer onClick={onClick}>{title}</CursorPointer> : <span>{title}</span>
+}
+
+function useRenameMode(state) {
+  const [renameMode, setRenameMode] = useState(state)
+
+  function toggleRenameMode() {
+    setRenameMode(!renameMode)
+  }
+
+  return [renameMode, toggleRenameMode]
+}
+
+export default function({ children: column, allowRemoveColumn, onColumnRemove, allowRenameColumn, onColumnRename }) {
+  const [renameMode, toggleRenameMode] = useRenameMode(false)
+  const [titleInput, setTitleInput] = useState('')
+
+  function handleRenameColumn(event) {
+    event.preventDefault()
+
+    onColumnRename(column, titleInput)
+    toggleRenameMode()
+  }
+
+  function handleRenameMode() {
+    setTitleInput(column.title)
+    toggleRenameMode()
+  }
+
+  return (
+    <ColumnHeaderSkeleton>
+      {renameMode ? (
+        <form onSubmit={handleRenameColumn}>
+          <span>
+            <Input
+              type='text'
+              value={titleInput}
+              onChange={({ target: { value } }) => setTitleInput(value)}
+              autoFocus
+            />
+          </span>
+          <span>
+            <DefaultButton type='submit'>Rename</DefaultButton>
+            <DefaultButton type='button' onClick={handleRenameMode}>
+              Cancel
+            </DefaultButton>
+          </span>
+        </form>
+      ) : (
+        <>
+          <ColumnTitle allowRenameColumn={allowRenameColumn} onClick={handleRenameMode}>
+            {column.title}
+          </ColumnTitle>
+          {allowRemoveColumn && <span onClick={() => onColumnRemove(column)}>×</span>}
+        </>
+      )}
+    </ColumnHeaderSkeleton>
+  )
+}
